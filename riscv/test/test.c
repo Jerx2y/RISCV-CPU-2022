ff10ab019b8c93534b0be4e8f94e758445d8db4f
Jerx2y <jerx2y@outlook.com>
2022-12-25 10:55:29 +0800

    sim passed

--- riscv/test/test.c
+++ riscv/test/test.c
@@ -1,31 +1,11 @@
 #include "io.h"
-int cd(int d, char* a, char* b, char* c, int sum) {
-    sleep(5); // to prevent UART buffer from overflowing
-    if (d == 1) {
-        print("move ");
-        print(a);
-        print(" --> ");
-        println(c);
-        sum++;
-    } else {
-        sum = cd(d - 1, a, c, b, sum);
-        print("move ");
-        print(a);
-        print(" --> ");
-        println(c);
-        sum = cd(d - 1, b, a, c, sum);
-        sum++;
-    }
-    return sum;
+int a(int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9, int a10, int a11, int a12, int a13, int a14, int a15)
+{
+    return a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 + a9 + a10 + a11 + a12 + a13 + a14 + a15;
 }
 
-int main() {
-    char a[5] = "A";
-	char b[5] = "B";
-	char c[5] = "C";
-    int d = 10;
-    int sum = cd(d, a, b, c, 0);
-    outlln(sum);
+int main()
+{
+    outlln(a(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15));
     return 0;
 }
-
